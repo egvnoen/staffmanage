8710c1237f94c95660d3494bfbae9c7e929f865c
egvnoen <1227574108@qq.com>
2021-08-07 21:10:47 +0800

    简单实现登录以及拦截

--- src/main/java/com/bobo/config/MyMvcConfig.java
+++ src/main/java/com/bobo/config/MyMvcConfig.java
@@ -3,6 +3,7 @@ package com.bobo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,11 +14,19 @@ public class MyMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboard");
     }
 
     // 自定义的国家化组件
     @Bean
     public LocaleResolver localeResolver() {
         return new MyLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index.html", "/", "/user/login", "/css/*", "/js/**", "/img/**");
     }
 }
---
+++ src/main/java/com/bobo/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.bobo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/user/login")
+    public String login(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            Model model,
+            HttpSession httpSession) {
+
+        // 具体业务：
+        if ("admin".equals(username) && "123456".equals(password)) {
+            httpSession.setAttribute("loginUser", username);
+            return "redirect:/main.html";
+        } else {
+
+            // 登录失败
+            model.addAttribute("msg", "用户名或密码错误");
+            return "index";
+        }
+    }
+
+}
